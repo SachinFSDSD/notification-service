3a7a35696ec5eb38985e2174bbd5b3dcf1b8841c
SachinFSDSD <rajusachin090@gmail.com>
2023-09-06 20:23:10 +0530

    updated

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_NAME: "notification_db",
-  DB_URL: "mongodb+ srv://Sachin:welcom123@crm-backend.snwrxbq.mongodb.net/",
+  DB_URL: "mongodb+srv://rajusachin090:Welcom1@cluster0.vahqcu0.mongodb.net/",
 
   //"mongodb://localhost/notification_db",
 };
